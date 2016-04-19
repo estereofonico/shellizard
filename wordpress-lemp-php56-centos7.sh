ca4e705e95282b958f95c111d7f2668aa2587303
saisyukusanagi <saisyukusanagi@gmail.com>
2016-04-19 14:50:28 -0500

    Fix RPM installation

--- wordpress-lemp-php56-centos7.sh
+++ wordpress-lemp-php56-centos7.sh
@@ -9,9 +9,9 @@ set -eux
 sed -i s/SELINUX=enforcing/SELINUX=disabled/g /etc/sysconfig/selinux /etc/selinux/config
 
 # Enable external YUM repos
-rpm -FUvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-rpm -FUvh https://centos7.iuscommunity.org/ius-release.rpm
-rpm -FUvh https://repo.mysql.com/mysql57-community-release-el7-8.noarch.rpm
+yum -y reinstall https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum -y reinstall https://centos7.iuscommunity.org/ius-release.rpm
+yum -y reinstall https://repo.mysql.com/mysql57-community-release-el7-8.noarch.rpm
 
 # System update
 yum -y update
