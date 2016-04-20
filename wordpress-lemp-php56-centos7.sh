4cde1282d61b29cd8256fad3669ab8529633107a
saisyukusanagi <saisyukusanagi@gmail.com>
2016-04-19 22:43:02 -0500

    WP Installer

--- wordpress-lemp-php56-centos7.sh
+++ wordpress-lemp-php56-centos7.sh
@@ -89,9 +89,9 @@ fi
 #if [ -d /var/lib/mysql/wordpress ]; then
 #   echo "Database already exists"
 #else
-   WPMYPASS=$(pwgen -1 -B 14)
+   WPMYPASS=$(pwgen -1 -yncB 14)
 #   echo "CREATE DATABASE wordpress;" | mysql
-   echo "GRANT ALL PRIVILEGES ON wordpress.* TO wordpress IDENTIFIED BY $WPMYPASS;" | mysql
+   echo "GRANT ALL PRIVILEGES ON wordpress.* TO wordpress IDENTIFIED BY \'$WPMYPASS\';" | mysql
    echo "FLUSH PRIVILEGES;" | mysql
 #fi
 
