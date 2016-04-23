73e780fd386a3ff0b67452eea3daaddd1d3c1443
saisyukusanagi <saisyukusanagi@gmail.com>
2016-04-22 20:31:40 -0500

    Dropped MySQL

--- mysql57-single-centos7.sh
+++ mysql57-single-centos7.sh
@@ -16,6 +16,7 @@ sed -i s/SELINUX=enforcing/SELINUX=disabled/g /etc/sysconfig/selinux /etc/selinu
 # Enable external YUM repos
 rpm -q epel-release || yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 rpm -q ius-release  || yum -y install https://centos7.iuscommunity.org/ius-release.rpm
+rpm -q mysql57-community-release || yum -y install https://repo.mysql.com/mysql57-community-release-el7-8.noarch.rpm
 
 # System update
 yum -y update
@@ -33,6 +34,9 @@ yum -y install nginx
 # PHP
 yum -y install php56u-fpm-nginx php56u-mysql
 
+# MySQL
+yum -y install mysql-community-server
+
 
 ##########################
 ## Services configuration
@@ -41,17 +45,15 @@ yum -y install php56u-fpm-nginx php56u-mysql
 sed -i 's/\/usr\/share\/nginx\/html/\/var\/nginx\/html/g' /etc/nginx/nginx.conf
 mkdir -p /var/nginx
 
-# Change php-fpm exec user
-sed -i 's/user\ \=\ php-fpm/user\ \=\ nginx/g' /etc/php-fpm.d/www.conf
-sed -i 's/group\ \=\ php-fpm/user\ \=\ nginx/g' /etc/php-fpm.d/www.conf
-
 # Enable services
 sudo systemctl enable  php-fpm
 sudo systemctl enable  nginx
+sudo systemctl enable  mysqld
 
 # Start services
 sudo systemctl restart php-fpm
 sudo systemctl restart nginx
+sudo systemctl restart mysqld
 
 #######################
 ## MySQL Configuration
@@ -73,5 +75,25 @@ else
    curl https://wordpress.org/latest.tar.gz -o /var/nginx/wp-installer.tar.gz -L
    tar -xvzf /var/nginx/wp-installer.tar.gz -C /var/nginx
    mv /var/nginx/wordpress /var/nginx/html
-   chown nginx: /var/nginx/html
+   chown php-fpm: /var/nginx/html
 fi
+
+if [ -d /var/lib/mysql/wordpress ]; then
+   echo "Database already exists"
+else
+   WPMYPASS=$(pwgen -1 -B 14)
+   echo "CREATE DATABASE wordpress;" | mysql
+fi
+
+
+#if [ -d /var/lib/mysql/wordpress ]; then
+#   echo "Database already exists"
+#else
+   WPMYPASS=$(pwgen -1 -yncB 14)
+#   echo "CREATE DATABASE wordpress;" | mysql
+   echo "GRANT ALL PRIVILEGES ON wordpress.* TO wordpress IDENTIFIED BY "\'$WPMYPASS\'";" | mysql
+   echo "FLUSH PRIVILEGES;" | mysql
+#fi
+
+echo "Your Wordpress MySQL user is: wordpress"
+echo "Your Wordpress MySQL password is: $WPMYPASS"
